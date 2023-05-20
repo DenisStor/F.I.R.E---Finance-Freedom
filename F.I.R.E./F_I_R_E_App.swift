0a9d1e6a7ec83e05ad0f09413935e5130abfb7ad
DenShizik <denisdikarvit@gmail.com>
2023-05-20 15:55:57 +0200

    Initial Commit

---
+++ F.I.R.E./F_I_R_E_App.swift
@@ -0,0 +1,17 @@
+//
+//  F_I_R_E_App.swift
+//  F.I.R.E.
+//
+//  Created by Денис Сторожев on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct F_I_R_E_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
