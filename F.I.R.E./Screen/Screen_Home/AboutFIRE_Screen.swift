8ff288f43088073d9eb585ace3d12747c58d5fb7
Nikita Malykhin <malykhinwork@internet.ru>
2023-05-21 17:29:32 +0200

    aboutFire

--- F.I.R.E./Screen/Screen_Home/AboutFIRE_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/AboutFIRE_Screen.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct AboutFIRE_Screen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("Color_back")
+                .edgesIgnoringSafeArea(.all)
+            
+            ScrollView (showsIndicators: false){
+                VStack(alignment: .leading) {
+                    
+                    Text("Fire_3")
+                        .font(.system(size: 65.5, weight: .semibold)).foregroundColor(Color("Color_font_1"))
+                        .padding(.bottom, 10)
+                    
+                    Text("Fire_1")
+                        .font(.system(size: 23.4, weight: .medium))
+                        .padding(.bottom, 20)
+                        .lineSpacing(5)
+                        .foregroundColor(Color("Color_font"))
+                    
+                    Text("Fire_2")
+                        .font(.system(size: 23.4, weight: .medium))
+                        .lineSpacing(5)
+                        .foregroundColor(Color("Color_font"))
+                        .padding(.bottom,30)
+                  
+                    Image("logo_Fire")
+
+                    
+                } .padding(15)
+            }
+        }
     }
 }
 
