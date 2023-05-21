004a9c3adf0abbdb9f6d171dd0c19b12b5acca5c
Nikita Malykhin <malykhinwork@internet.ru>
2023-05-21 22:28:30 +0200

    traty

--- F.I.R.E./Screen/Calculatro_Views/SafeMoney_Screen.swift
+++ F.I.R.E./Screen/Calculatro_Views/SafeMoney_Screen.swift
@@ -8,13 +8,139 @@
 import SwiftUI
 
 struct SafeMoney_Screen: View {
+    
+    @StateObject var data = spendmoney()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        @AppStorage ("earnMoney") var earnMoney : Double = 10
+        @AppStorage ("spendMoney") var spendMoney : Double = 10
+        
+        
+        ZStack {
+            VStack{
+                
+                ZStack {
+                    
+                    RoundedRectangle(cornerRadius: 30.71)
+                        .foregroundColor(Color("Color_font_1"))
+                        .frame(height: 179)
+                    VStack {
+                        HStack {
+                            
+                            Text("Сколько вы зарабатываете")
+                                .font(.system(size: 33, weight: .medium)).foregroundColor(Color("Color_font_2"))
+                                .padding(.horizontal,15)
+                            
+                            Spacer()
+                            
+                        }
+                        HStack {
+                            HStack {
+                                
+                                TextField("в месяц",value: $earnMoney, format: .number)
+                                    .font(.system(size: 33))
+                                    .foregroundColor(Color("Color_procent_text"))
+                                
+                                    .background{
+                                        
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .foregroundColor(Color("Color_font_2"))
+                                        
+                                        
+                                    }
+                                Text("$")
+                                    .font(.system(size: 33,weight: .medium))
+                                    .foregroundColor(Color("Color_font_2"))
+                                
+                                Spacer()
+                                
+                                
+                                
+                            }
+                            .padding(.horizontal,15)
+                            
+                        }
+                    }
+                    
+                }
+                
+                ZStack {
+                    
+                    RoundedRectangle(cornerRadius: 30.71)
+                        .foregroundColor(Color("Color_button"))
+                        .frame(height: 179)
+                    VStack {
+                        HStack {
+                            
+                            Text("Обязательные трат в месяц ")
+                                .font(.system(size: 33, weight: .medium)).foregroundColor(Color("Color_font"))
+                                .padding(.horizontal,15)
+                            
+                            Spacer()
+                            
+                        }
+                        HStack {
+                            HStack {
+                                
+                                TextField("в месяц",value: $spendMoney, format: .number)
+                                    .font(.system(size: 33))
+                                    .foregroundColor(Color("Color_procent_text"))
+                                
+                                    .background{
+                                        
+                                        RoundedRectangle(cornerRadius: 15)
+                                            .foregroundColor(Color("Color_font_2"))
+                                        
+                                        
+                                    }
+                                Text("$")
+                                    .font(.system(size: 33,weight: .medium))
+                                    .foregroundColor(Color("Color_font"))
+                                
+                                Spacer()
+                                
+                                
+                                
+                            }
+                            
+                            
+                            
+                            .padding(.horizontal,15)
+                            
+                        }
+                    }
+                    
+                }
+                Spacer()
+                ZStack {
+                    
+                    VStack {
+                        HStack {
+                            Text("1 321₽").font(.system(size: 117.2, weight: .medium)).foregroundColor(Color("Color_font_1"))
+                            Spacer()
+                        }
+                        
+                        
+                    }
+                }
+                
+                ZStack {
+                    VStack {
+                        HStack {
+                            Text("Вы можете потратить в день").font(.system(size: 44.9, weight: .medium))
+                            Spacer()
+                        }
+                    }
+                }
+                    
+                }.padding(10)
+            }
+        }
+        struct SafeMoney_Screen_Previews: PreviewProvider {
+            static var previews: some View {
+                SafeMoney_Screen()
+            }
+        }
+        
     }
-}
-
-struct SafeMoney_Screen_Previews: PreviewProvider {
-    static var previews: some View {
-        SafeMoney_Screen()
-    }
-}
---
+++ F.I.R.E./Screen/SpendMoney.swift
@@ -0,0 +1,20 @@
+//
+//  SpendMoney.swift
+//  F.I.R.E.
+//
+//  Created by Никита Малыхин on 21.05.2023.
+//
+
+import Foundation
+
+final class spendmoney : ObservableObject {
+    
+//
+//    Доходы - расходы / 30.4
+//    
+//
+    
+    
+    
+    
+}
