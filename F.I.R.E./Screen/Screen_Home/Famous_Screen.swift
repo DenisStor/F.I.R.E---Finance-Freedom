b686bd51fd46b197f957cc1b2bb59d808bee6ee6
Nikita Malykhin <malykhinwork@internet.ru>
2023-05-23 17:56:49 +0200

    famous

--- F.I.R.E./Screen/Screen_Home/Famous_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/Famous_Screen.swift
@@ -10,35 +10,36 @@ import SwiftUI
 struct Famous_Screen: View {
     @State private var name : [String] = [
     NSLocalizedString("famous_name1", comment: ""),
-    NSLocalizedString("famous_name2", comment: ""),
-    NSLocalizedString("famous_name4", comment: ""),
     NSLocalizedString("famous_name3", comment: ""),
+    NSLocalizedString("famous_name4", comment: ""),
+    NSLocalizedString("famous_name2", comment: ""),
     NSLocalizedString("famous_name4", comment: ""),
     ]
     
     
     //их имена \n для разделения имени и фамилии
-    @State private var Image1 : [String] = ["Baffet","Bill","Elon","Pitere"] // картинки
+    @State private var Image1 : [String] = ["Baffet","Bill","Elon","Pitere"]
+    
     
     @State private var Color1 : [String] = ["Color_font_2","Color_font","Color_font","Color_font"]
     
     @State private var title : [String] = [
     NSLocalizedString("famous_name1", comment: ""),
-    NSLocalizedString("famous_name2", comment: ""),
-    NSLocalizedString("famous_name4", comment: ""),
     NSLocalizedString("famous_name3", comment: ""),
+    NSLocalizedString("famous_name4", comment: ""),
+    NSLocalizedString("famous_name2", comment: ""),
     NSLocalizedString("word_4", comment: ""),
     ]
     
     
     
     //заголовок статьи
-    @State private var Image2 : [String] = ["Baffet","Bill","Elon","Pitere"] // картинка
+    @State private var Image2 : [String] = ["baffet1scr","bill2scr","elon2scr","litch2scr"] // картинка
     @State private var Text1 : [String] = [
-        NSLocalizedString("famous_name1", comment: ""),
-        NSLocalizedString("word_1", comment: ""),
-        NSLocalizedString("word_2", comment: ""),
-        NSLocalizedString("word_3", comment: ""),
+        NSLocalizedString("famouse1text", comment: ""),
+        NSLocalizedString("fam3text", comment: ""),
+        NSLocalizedString("fam4text", comment: ""),
+        NSLocalizedString("fam2text", comment: ""),
         NSLocalizedString("word_4", comment: ""),
         ] // основной текст
     
@@ -48,7 +49,7 @@ struct Famous_Screen: View {
                 .edgesIgnoringSafeArea(.all)
             NavigationStack{
                 ScrollView(showsIndicators: false){
-                    LazyVStack(spacing: 5){
+                    LazyVStack(spacing: 15){
                         ForEach(0..<4) { index in
                             NavigationLink {
                                 ZStack{
@@ -58,17 +59,21 @@ struct Famous_Screen: View {
                                         LazyVStack(alignment: .leading){
                                             Text(title[index])
                                                 .font(.system(size: 53, weight: .medium)).foregroundColor(Color("Color_font_1"))
+                                                .padding(.horizontal, 20)
                                             Image(Image2[index])
                                                 .resizable()
                                                 .aspectRatio(contentMode: .fit)
                                             
                                             Text(Text1[index])
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
                                      
                                                 
                                             
                                             
                                         }
-                                    }.padding(.horizontal,15)
+                                    }
                                 }
                                 
                             } label: {
