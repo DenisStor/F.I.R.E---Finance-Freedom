16af147465c4951735e1ea58461fcb3b9aa4cb4f
DenShizik <denisdikarvit@gmail.com>
2023-05-23 16:43:45 +0200

    fix

--- F.I.R.E./Screen/Settings_screen.swift
+++ F.I.R.E./Screen/Settings_screen.swift
@@ -102,7 +102,7 @@ struct Settings_screen: View {
         if selectedTheme == "dark" {
             return .dark
         } else if selectedTheme == "system" {
-            return colorScheme == .dark ? .light : .dark
+            return colorScheme == .light ? .light : .dark
         } else {
             return .light
         }
