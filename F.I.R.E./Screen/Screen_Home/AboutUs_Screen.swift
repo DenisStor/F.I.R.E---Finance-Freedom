6b7883f384847819836d12e3d75919078f396d54
DenShizik <denisdikarvit@gmail.com>
2023-05-23 17:44:40 +0200

    fix

--- F.I.R.E./Screen/Screen_Home/AboutUs_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/AboutUs_Screen.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct AboutUs_Screen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("Color_back")
+                .edgesIgnoringSafeArea(.all)
+            ScrollView{
+                
+                HStack {
+                    VStack(alignment: .leading){
+                        
+                        Image("banana_Image")
+                        Text("Разработано")
+                            .font(.system(size: 40,weight: .medium))
+                            .foregroundColor(Color("Color_font"))
+                        Text("Banana project")
+                            .font(.system(size: 40,weight: .medium))
+                            .foregroundColor(Color("Color_banana"))
+                        Text("перейти на сайт")
+                            .font(.system(size: 26,weight: .medium))
+                            .foregroundColor(Color("Color_font_3"))
+                            
+                        Text("Советы и \nпредложения")
+                            .font(.system(size: 34,weight: .medium))
+                            .foregroundColor(Color("Color_font"))
+                            .padding(.top,100)
+                        Text("support@banana.ru")
+                            .font(.system(size: 34,weight: .medium))
+                            .tint(Color("Color_font_3"))
+                        
+                        
+                    }//.frame(height: 800)
+                    .padding(.horizontal,20)
+                        .multilineTextAlignment(.leading)
+                      
+                    Spacer()
+                }.padding(5)
+                
+            }
+            
+        }
     }
 }
 
