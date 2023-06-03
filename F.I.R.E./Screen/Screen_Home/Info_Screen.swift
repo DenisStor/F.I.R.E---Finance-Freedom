2dc8ae08820592d99eddb1ec30781560bb6131e3
DenShizik <denisdikarvit@gmail.com>
2023-06-03 22:29:06 +0200

    арч

--- F.I.R.E./Screen/Screen_Home/Info_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/Info_Screen.swift
@@ -1,12 +1,5 @@
 //
-//  Info_Screen.swift
-//  F.I.R.E.
-//
-//  Created by Денис Сторожев on 03.06.2023.
-//
-
-import SwiftUI
-
+//  Info_Scre
 //
 //  Famous_Screen.swift
 //  F.I.R.E.
@@ -18,16 +11,16 @@ import SwiftUI
 
 struct info_Screen: View {
     @State private var name : [String] = [
-    NSLocalizedString("famous_name1", comment: ""),
-    NSLocalizedString("famous_name3", comment: ""),
-    NSLocalizedString("famous_name4", comment: ""),
-    NSLocalizedString("famous_name2", comment: ""),
-    NSLocalizedString("famous_name4", comment: ""),
+    NSLocalizedString("famous_name1", comment: ""), // облигации
+    NSLocalizedString("famous_name3", comment: ""), // акции
+    NSLocalizedString("famous_name4", comment: ""), // крипта
+    NSLocalizedString("famous_name2", comment: ""), // недвижимость
+    
     ]
     
     
     //их имена \n для разделения имени и фамилии
-    @State private var Image1 : [String] = ["Baffet","Bill","Elon","Pitere"]
+    @State private var Image_main : [String] = ["Baffet","Bill","Elon","Pitere"]
     
     
     @State private var Color1 : [String] = ["Color_font_2","Color_font","Color_font","Color_font"]
@@ -37,7 +30,7 @@ struct info_Screen: View {
     NSLocalizedString("famous_name3", comment: ""),
     NSLocalizedString("famous_name4", comment: ""),
     NSLocalizedString("famous_name2", comment: ""),
-    NSLocalizedString("word_4", comment: ""),
+    
     ]
     
     
@@ -74,19 +67,40 @@ struct info_Screen: View {
                                     ScrollView{
                                         
                                         VStack(alignment: .leading){
-                                            Text(title[index])
-                                                .font(.system(size: 53, weight: .medium)).foregroundColor(Color("Color_font_1"))
-                                                .padding(.horizontal, 20)
-                                            Image(Image2[index])
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                            
-                                            Text(Text1[index])
+                                            Text("Акции")
                                                 .font(.system(size: 20.4, weight: .medium))
                                                 .lineSpacing(10)
                                                 .padding(20)
-                                     
-                                                
+                                            Text("Image")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
+                                            
+                                            Text("Что такое акция")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
+                                            
+                                            Text("Акция — это ценная бумага, которую выпускает акционерное общество, другими словами — компания-эмитент. Все инвесторы, которые купили акции, стали совладельцами компании. Акция как раз и подтверждает, что у ее владельца есть доля в компании, пусть даже совсем маленькая.")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
+                                            Text("Какие бывают виды акций?")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
+                                            Text("text")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
+                                            Text("Где можно купить акции?")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
+                                            Text("На бирже или вне биржи. Торговля на бирже более прозрачна — котировки акций и других ценных бумаг можно легко отследить. Когда вы покупаете или продаете акции напрямую, вне биржи, есть риск, что цены будут завышены или занижены по сравнению с рыночными.")
+                                                .font(.system(size: 20.4, weight: .medium))
+                                                .lineSpacing(10)
+                                                .padding(20)
                                             
                                             
                                         }
@@ -99,7 +113,7 @@ struct info_Screen: View {
                                 ZStack{
                                     
                                     
-                                    Image(Image1[index])
+                                    Image(Image_main[index])
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
                                     VStack{
