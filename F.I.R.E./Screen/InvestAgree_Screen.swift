8886176b7eb1f168990d4b8fabe0c14c11b8df9c
Nikita Malykhin <malykhinwork@internet.ru>
2023-06-07 21:49:57 +0200

    bjhjhj

--- F.I.R.E./Screen/InvestAgree_Screen.swift
+++ F.I.R.E./Screen/InvestAgree_Screen.swift
@@ -10,18 +10,13 @@ import SwiftUI
 struct InvestAgree_Screen: View {
     
     @State private var name : [String] = [
-    NSLocalizedString("Акции", comment: ""),
-    NSLocalizedString("Облигации", comment: ""),
-    NSLocalizedString("Криптовалюта", comment: ""),
-    NSLocalizedString("Недвижимость", comment: ""),
+    NSLocalizedString("stockmenu", comment: ""),
+    NSLocalizedString("obligimenu", comment: ""),
+    NSLocalizedString("cryptomenu", comment: ""),
+    NSLocalizedString("nedviga", comment: ""),
     ]
     
-    @State private var text1 : [String] = [
-    NSLocalizedString("Test", comment: ""),
-    NSLocalizedString("Yujds", comment: ""),
-    NSLocalizedString("Криптовалюта", comment: ""),
-    NSLocalizedString("Недвижимость", comment: ""),
-    ]
+
     @State private var IsOn : Int = 0
     @State private var clr : [Bool] = [true , false , false , false]
   
@@ -33,7 +28,7 @@ struct InvestAgree_Screen: View {
             ScrollView(showsIndicators: false){
                 VStack (alignment: .leading, spacing: 20) {
                     HStack{
-                        Text("Инвестируем\nправильно")
+                        Text("Investagree1")
                             .font(.system(size: 40, weight: .semibold))
                             .foregroundColor(Color("Color_font_1"))
                         Spacer()
@@ -80,14 +75,14 @@ struct InvestAgree_Screen: View {
                         if IsOn == 0 {
                             
                             HStack{
-                                Text("Облигации — долговая расписка, ее выпускает эмитент, которому нужны средства. Покупая эти бумаги, инвестор дает свои деньги взаймы, за что получает доход")
+                                Text("Promotion1")
                                     .font(.system(size: 23, weight: .semibold))
                                 Spacer()
                             }
                             
                             HStack{
                            
-                                Text("Шаг 1")
+                                Text("ConservativeStep1")
                                     .font(.system(size: 40, weight: .semibold))
                                     .foregroundColor(Color("Color_font_1"))
                                     .padding(.top, 10)
@@ -97,31 +92,16 @@ struct InvestAgree_Screen: View {
                             }
                             HStack{
                            
-                                Text("Прежде чем приобрести облигации того или иного эмитента, ознакомьтесь с кредитными рейтингами, которые ему присвоены.")
+                                Text("ConservativeStep1.1")
                                     .font(.system(size: 23, weight: .medium))
                                     .padding(.bottom,10)
                                 Spacer()
                                 
                             }
-                            
-                            HStack{
-                                VStack (alignment: .leading) {
-                                    Text("Кредитные рейтинги:")
-                                        .font(.system(size: 16.8, weight:.medium))
-                                        .foregroundColor(Color("Color_font_3"))
-                                    
-                                    Image("creditAa")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("чем ниже кредитный рейтинг, тем больше вероятность дефолта.")
-                                        .font(.system(size: 16.8, weight:.medium))
-                                        .foregroundColor(Color("Color_font_3"))
-                                }
-                                Spacer()
-                            }
+                  
                             HStack{
                            
-                                Text("Шаг 2")
+                                Text("ConservativeStep2")
                                     .font(.system(size: 40, weight: .semibold))
                                     .foregroundColor(Color("Color_font_1"))
                                     .padding(.top, 10)
@@ -132,7 +112,7 @@ struct InvestAgree_Screen: View {
                             
                             HStack{
                            
-                                Text("Убедиться, что этому эмитенту можно доверить свои средства, помогут не только кредитные рейтинги, но и представление о его деятельности — основные сведения можно подчерпнуть из презентаций компании и регулярной отчетности.")
+                                Text("ConservativeStep2.1")
                                     .font(.system(size: 23, weight: .medium))
                                     .padding(.bottom,10)
                                 Spacer()
@@ -141,7 +121,7 @@ struct InvestAgree_Screen: View {
                             
                             HStack{
                            
-                                Text("Шаг 3")
+                                Text("Step3")
                                     .font(.system(size: 40, weight: .semibold))
                                     .foregroundColor(Color("Color_font_1"))
                                     .padding(.top, 10)
@@ -152,7 +132,7 @@ struct InvestAgree_Screen: View {
                             
                             HStack{
                            
-                                Text("Они изучают эмитента, могут предоставлять наглядные таблицы и графики, то есть экономят ваши усилия и время при поиске необходимой информации.")
+                                Text("AggressiveStep2.2")
                                     .font(.system(size: 23, weight: .medium))
                                     .padding(.bottom,10)
                                 Spacer()
@@ -163,13 +143,255 @@ struct InvestAgree_Screen: View {
                             
                         }
                         if IsOn == 1 {
-                            Text("обилиги")
+                            
+                            HStack{
+                                Text("Bonds2")
+                                    .font(.system(size: 23, weight: .semibold))
+                                Spacer()
+                            }
+                            
+                            HStack{
+                           
+                                Text("Step1")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            HStack{
+                           
+                                Text("Step1.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                            HStack{
+                                VStack (alignment: .leading) {
+                                    Text("Creditratings1")
+                                        .font(.system(size: 16.8, weight:.medium))
+                                        .foregroundColor(Color("Color_font_3"))
+                                    
+                                    Image("creditAa")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                    Text("Creditratings2")
+                                        .font(.system(size: 16.8, weight:.medium))
+                                        .foregroundColor(Color("Color_font_3"))
+                                }
+                                Spacer()
+                            }
+                            HStack{
+                           
+                                Text("Step2")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("Step2.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("Step3")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("Step3.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("Step3.2")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
                         }
                         if IsOn == 2 {
-                            Text("крипта")
+                            HStack{
+                                Text("Cryptocurrency2")
+                                    .font(.system(size: 23, weight: .semibold))
+                                Spacer()
+                            }
+                            
+                            HStack{
+                           
+                                Text("CryptoStep1")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            HStack{
+                           
+                                Text("CryptoStep1.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                 
+                            HStack{
+                           
+                                Text("CryptoStep2")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("CryptoStep2.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("CryptoObjectivesInvestments1")
+                                    .font(.system(size: 30, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_3"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("CryptoObjectivesInvestments2")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                  
                         }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                        
                         if IsOn == 3 {
-                            Text("недвига")
+                            HStack{
+                                Text("EstateInvestments1")
+                                    .font(.system(size: 23, weight: .semibold))
+                                Spacer()
+                            }
+                            
+                            HStack{
+                           
+                                Text("EstateStep1")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            HStack{
+                           
+                                Text("EstateStep1.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                 
+                            HStack{
+                           
+                                Text("EstateStep2")
+                                    .font(.system(size: 40, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_1"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("EstateStep2.1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("EstateInvestments1.2")
+                                    .font(.system(size: 30, weight: .semibold))
+                                    .foregroundColor(Color("Color_font_3"))
+                                    .padding(.top, 10)
+                                Spacer()
+                                    
+                                
+                            }
+                            
+                            HStack{
+                           
+                                Text("EstateAdvice1")
+                                    .font(.system(size: 23, weight: .medium))
+                                    .padding(.bottom,10)
+                                Spacer()
+                                
+                            }
+                            
+
                         }
                     }.padding(.horizontal,15)
                     
