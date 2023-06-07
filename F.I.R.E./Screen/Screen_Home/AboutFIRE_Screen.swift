fe1b6774368ee620b875ba458a48083c4039a1c0
Nikita Malykhin <malykhinwork@internet.ru>
2023-06-07 15:07:49 +0200

    vfdsd

--- F.I.R.E./Screen/Screen_Home/AboutFIRE_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/AboutFIRE_Screen.swift
@@ -9,28 +9,79 @@ import SwiftUI
 
 struct AboutFIRE_Screen: View {
     var body: some View {
-        NavigationStack{
-            ZStack{
-                Color("Color_back")
-                    .edgesIgnoringSafeArea(.all)
-                ScrollView(showsIndicators: false){
-                  
-                    
-                    ZStack{
+        ZStack {
+            Color("Color_back")
+                .edgesIgnoringSafeArea(.all)
+            ScrollView(showsIndicators: false){
+                VStack {
+                    HStack {
                         
-                        HStack{
+                        VStack (alignment: .leading){
                             Image("logoFire")
+                            Text("FIRE как образ\nжизни")
+                                .font(.system(size: 37.5, weight: .semibold))
+                                .foregroundColor(Color("Color_font_1"))
+                                .padding(.top, 10)
+                                .padding(.bottom,5)
+                            
+                            Text("Движение FIRE появилось в США в 90-х годах, а его основные идеи получили развитие благодаря книге 1992 года «Кошелек или жизнь»")
+                                .font(.system(size: 23, weight: .semibold))
+                                .lineSpacing(4)
+                                .padding(.vertical,10)
+                                
+                                Text("“Время жизни ценнее материальных вещей”")
+                                .font(.system(size: 30, weight: .semibold))
+                                
+                            Spacer()
+                            
+                            
                         }
-                        VStack{
-                            Text("FIRE как образ жизни")
-                        }
+                        
                     }
                     
                     
                     
+                    ZStack {
+                        ZStack {
+                            
+                            VStack(alignment: .leading) {
+                                ZStack {
+                                    VStack (alignment: .leading){
+                                        
+                                        HStack {
+                                            Text("Чтобы скопить капитал и наконец перестать «работать ради жизни», авторы призывают быть бережливыми, контролировать расходы и помнить, что сбережения должны покрывать будущие расходы.")
+                                                .font(.system(size: 23, weight: .semibold))
+                                                .foregroundColor(Color("Color_font_2"))
+                                                .lineSpacing(3)
+                                            Spacer()
+                                            
+                                            
+                                            
+                                        }
+                                    }
+                                }
+                            }    .padding(20)
+                            .background(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .foregroundColor(Color("Color_font_1"))
+                       
+                                 
+                            )  }.padding(.top, 10)
+                    }
+                    .padding(.top,10)
+                
                     
+                    Text("Драйвером FIRE стал резкий рост цен на недвижимость в США в 2000-х годах. Молодые люди поняли, что не потянут покупку квартиры.")
+                        .font(.system(size: 23, weight: .semibold))
+                        .padding(.top,20)
+                        .lineSpacing(4)
+                    Image("grafik")
+                        .padding(.top,30)
+                  
                 }
-            }
+                
+               
+            } .padding(.horizontal,15)
         }
     }
 }
