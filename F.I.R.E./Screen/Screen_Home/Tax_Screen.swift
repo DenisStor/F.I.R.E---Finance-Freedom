38578f0a168b02cbfad8ac1dd25006534091259b
Nikita Malykhin <malykhinwork@internet.ru>
2023-06-11 13:29:17 +0200

    tax

--- F.I.R.E./Screen/Screen_Home/Tax_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/Tax_Screen.swift
@@ -9,146 +9,94 @@ import SwiftUI
 
 struct Tax_Screen: View {
     var body: some View {
-        ZStack {
+        ZStack{
             Color("Color_back")
                 .edgesIgnoringSafeArea(.all)
             ScrollView(showsIndicators: false){
-                VStack {
-                    HStack {
+                VStack (alignment: .leading, spacing: 15) {
+                    HStack{
+                        Text("nalogiName")
+                            .font(.system(size: 37.5, weight: .semibold))
+                            .foregroundColor(Color("Color_font_1"))
+                        Spacer()
+                    }
+                    
+                    HStack{
+                        Text("nalog1")
+                            .font(.system(size: 23, weight: .semibold))
+                            .lineSpacing(4)
                         
-                        VStack (alignment: .leading){
-                            
-                            Text("nalogiName")
-                                .font(.system(size: 37.5, weight: .semibold))
+                    }
+                    
+                    Divider()
+                        .frame(height: 2)
+                        .overlay(Color("Color_button"))
+                    
+                    
+                    Group {
+                        
+                        HStack {
+                            Text("znaknalogi")
+                                .font(.system(size: 59.5, weight: .semibold))
                                 .foregroundColor(Color("Color_font_1"))
-                                .padding(.top, 10)
-                                .padding(.bottom,5)
-                            
-                            Text("nalog1")
+                        }
+                        
+                        HStack{
+                            Text("textNalogi1")
                                 .font(.system(size: 23, weight: .semibold))
+                                .foregroundColor(Color("Color_font"))
                                 .lineSpacing(4)
-                            
-                                
-                                
-                            Spacer()
-                            
-                            
                         }
                         
                     }
-                    
-                    
-                    
-                    ZStack {
-                        ZStack {
-                            
-                            VStack(alignment: .leading) {
-                                
-                                HStack {
-                                    
-                                    Text("znaknalogi")
-                                    
-                                    
-                                        .font(.system(size: 60, weight: .medium))
-                                        .foregroundColor(Color("Color_font_2"))
-                                    
-                                    Spacer()
-                                    
-                                    
-                                }
-                                ZStack {
-                                    VStack (alignment: .leading){
-                                        
-                                        HStack {
-                                            Text("textNalogi1")
-                                                .font(.system(size: 23, weight: .semibold))
-                                                .foregroundColor(Color("Color_font_2"))
-                                                .lineSpacing(3)
-                                            Spacer()
-                                            
-                                            
-                                            
-                                        }
-                                    }
-                                }
-                            }    .padding(20)
-                            .background(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .foregroundColor(Color("Color_font_1"))
-                                
-                                 
-                            )  }
-                    }
-                    .padding(.top,10)
-                    ZStack {
-                            
-                            VStack(alignment: .leading) {
-                                
-                                HStack {
-                                    Text("titleNalogi2")
-                                        .font(.system(size: 60, weight: .medium))
-                                        .foregroundColor(Color("Color_font_1"))
-                                    Spacer()
-                                }
-                                    VStack (alignment: .leading){
-                                        
-                                        HStack {
-                                            Text("textNalogi2")
-                                                .font(.system(size: 23, weight: .semibold))
-                                                .foregroundColor(Color("Color_font"))
-                                                .lineSpacing(3)
-                                        }
-                                    
-                                    }
-                                } .padding(20)
+                    Divider()
+                        .frame(height: 2)
+                        .overlay(Color("Color_button"))
+              
+                    Group {
                         
-                            .background(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .foregroundColor(Color("Color_button"))
-                                
-                                 
-                            )      .padding(.top,15) }
+                        HStack {
+                            Text("titleNalogi2")
+                                .font(.system(size: 59.5, weight: .semibold))
+                                .foregroundColor(Color("Color_font_1"))
+                        }
+                        
+                        HStack{
+                            Text("textNalogi2")
+                                .font(.system(size: 23, weight: .semibold))
+                                .foregroundColor(Color("Color_font"))
+                                .lineSpacing(4)
+                        }
+                    }
                     
-                    ZStack {
-                        ZStack {
-                            VStack(alignment: .leading) {
-                                HStack {
-                                    Text("titleNalogi3")
-                                        .font(.system(size: 60, weight: .medium))
-                                        .foregroundColor(Color("Color_font_1"))
-                                    Spacer()
-                                }
-                                    VStack (alignment: .leading){
-                                        
-                                        HStack {
-                                            Text("textNalogi3")
-                                                .font(.system(size: 23, weight: .semibold))
-                                                .foregroundColor(Color("Color_font"))
-                                                .lineSpacing(3)
-                                            Spacer()
-                                            
-                                            
-                                            
-                                        }
-                                    }
-                                
-                            } .padding(20)
-                            .background(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .foregroundColor(Color("Color_button"))
-                                 
-                            )      }
-                        .padding(.top,15)    }
+                    Divider()
+                        .frame(height: 2)
+                        .overlay(Color("Color_button"))
                     
-                }
-                
-                
-            } .padding(.horizontal,15)
+                    Group {
+                        
+                        HStack {
+                            Text("titleNalogi3")
+                                .font(.system(size: 59.5, weight: .semibold))
+                                .foregroundColor(Color("Color_font_1"))
+                        }
+                        
+                        HStack{
+                            Text("textNalogi3")
+                                .font(.system(size: 23, weight: .semibold))
+                                .foregroundColor(Color("Color_font"))
+                                .lineSpacing(4)
+                        }
+                    }
+
+                    
+                }.padding(25)
+            }
         }
     }
-}
-struct Tax_Screen_Previews: PreviewProvider {
-    static var previews: some View {
-        Tax_Screen()
+    struct Tax_Screen_Previews: PreviewProvider {
+        static var previews: some View {
+            Tax_Screen()
+        }
     }
 }
