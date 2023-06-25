2c5df3f19395bf554fa00c435a143ed722c0dc6b
DenShizik <denisdikarvit@gmail.com>
2023-06-25 22:39:19 +0200

    fix

--- F.I.R.E./Screen/Screen_Home/Test_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/Test_Screen.swift
@@ -153,17 +153,17 @@ let textTest = [
                             HStack {
                                 switch finishForFirst{
                                 case 9...12:
-                                    Text("Вы готовы делать рискованные вложения ради потенциального дополнительного дохода, но его")
+                                    Text("Это инвестор, который предпочитает стратегию инвестирования, характеризующуюся высоким уровнем риска и ориентацией на получение максимальной возможной доходности. Они готовы вкладывать свои средства во волатильные рынки и активы с высоким потенциалом роста, даже если это связано с возможностью больших потерь.")
                                         .font(.system(size: 25,weight: .medium))
                                         .foregroundColor(Color("Color_font"))
                                         .multilineTextAlignment(.leading)
                                 case 5...9:
-                                    Text("Вы готовы делать рискованные вложения ради потенциального дополнительного дохода, но его")
+                                    Text("это инвестор, который преследует баланс между риском и доходностью, не стремится к чрезмерно высоким доходам, но готов принять некоторую степень риска в своих инвестициях для достижения умеренного уровня доходности.")
                                         .font(.system(size: 25,weight: .medium))
                                         .foregroundColor(Color("Color_font"))
                                         .multilineTextAlignment(.leading)
                                 case 0...5:
-                                    Text("Вы готовы делать рискованные вложения ради потенциального дополнительного дохода, но его")
+                                    Text("это инвестор, который предпочитает низкий уровень риска и стабильность в своих инвестициях, с акцентом на сохранение капитала.")
                                         .font(.system(size: 25,weight: .medium))
                                         .foregroundColor(Color("Color_font"))
                                         .multilineTextAlignment(.leading)
@@ -174,48 +174,16 @@ let textTest = [
                                 Spacer()
                             }
                             .padding(.bottom,25)
-                            HStack {
-                                Text("Главная цель — ")
-                                    .font(.system(size: 35,weight: .semibold))
-                                    .foregroundColor(Color("Color_font_1"))
-                                    .multilineTextAlignment(.leading)
-                                Spacer()
-                            }
-                            HStack{
-                                switch finishForTwo{
-                                case 4...6:
-                                    Text("накопление капитала на горизонте в 15-20 лет")
-                                        .font(.system(size: 25,weight: .medium))
-                                        .foregroundColor(Color("Color_font"))
-                                        .multilineTextAlignment(.leading)
-                                case 2...4:
-                                    Text("Вы готовы делать рискованные вложения ради потенциального дополнительного дохода, но его")
-                                        .font(.system(size: 25,weight: .medium))
-                                        .foregroundColor(Color("Color_font"))
-                                        .multilineTextAlignment(.leading)
-                                case 0...2:
-                                    Text("Вы готовы делать рискованные вложения ради потенциального дополнительного дохода, но его")
-                                        .font(.system(size: 25,weight: .medium))
-                                        .foregroundColor(Color("Color_font"))
-                                        .multilineTextAlignment(.leading)
-                                default:
-                                    Text("Erore")
-                                }
-                                Text("накопление капитала на горизонте в 15-20 лет")
-                                    .font(.system(size: 25,weight: .medium))
-                                    .foregroundColor(Color("Color_font"))
-                                    .multilineTextAlignment(.leading)
-                                Spacer()
-                            }
-                            .padding(.bottom,25)
+                            
+                        
                             HStack {
                                 Text("Вам подойдут")
                                     .font(.system(size: 35,weight: .medium))
                                     .foregroundColor(Color("Color_font_1"))
                                     .multilineTextAlignment(.leading)
                                 Spacer()
-                            }
-                            VStack(spacing:20){
+                            }.padding(.bottom,10)
+                            VStack(spacing:40){
                                 
                                 VStack(spacing:10){
                                     
@@ -229,10 +197,25 @@ let textTest = [
                                         Spacer()
                                     }
                                     HStack {
-                                        Text("50%")
-                                            .font(.system(size: 25,weight: .medium))
-                                            .foregroundColor(Color("Color_font"))
-                                            .multilineTextAlignment(.leading)
+                                        switch finishForFirst{
+                                        case 9...12:
+                                            Text("Акции: Приблизительно n% (70-80%) портфеля можно выделить на инвестиции в акции компаний с высоким потенциалом роста, новаторские компании или активы на развивающихся рынках.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 5...9:
+                                            Text("Акции: Приблизительно n% (50%) портфеля может быть выделено на инвестиции в акции стабильных компаний с хорошими фундаментальными показателями и умеренным ростом.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 0...5:
+                                            Text("Дивидендные акции: Приблизительно n% (5-10%) портфеля можно выделить на инвестиции в акции компаний с устойчивыми дивидендными выплатами и долгой историей финансовой стабильности.р")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        default:
+                                            Text("Erore")
+                                        }
                                         Spacer()
                                     }
                                 }
@@ -245,26 +228,56 @@ let textTest = [
                                         Spacer()
                                     }
                                     HStack {
-                                        Text("50%")
-                                            .font(.system(size: 25,weight: .medium))
-                                            .foregroundColor(Color("Color_font"))
-                                            .multilineTextAlignment(.leading)
+                                        switch finishForFirst{
+                                        case 9...12:
+                                            Text("Облигации: Приблизительно n% (10-20%) портфеля можно выделить на более высокодоходные, но рискованные облигации, такие как корпоративные облигации с низким рейтингом или облигации развивающихся стран.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 5...9:
+                                            Text("Облигации: Приблизительно n% (30%) портфеля можно выделить на облигации среднего и высокого качества, которые обеспечивают стабильный доход и уменьшают риск.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 0...5:
+                                            Text("Облигации: Приблизительно n% (60-70%) портфеля можно выделить на инвестиции в надежные и стабильные облигации с высоким кредитным рейтингом и низким уровнем риска.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        default:
+                                            Text("Erore")
+                                        }
                                         Spacer()
                                     }
                                 }
                                 VStack(spacing:10){
                                     HStack {
-                                        Text("Недвижимость")
+                                        Text("Инвестиционные фонды")
                                             .font(.system(size: 30,weight: .medium))
                                             .foregroundColor(Color("Color_font"))
                                             .multilineTextAlignment(.leading)
                                         Spacer()
                                     }
                                     HStack {
-                                        Text("50%")
-                                            .font(.system(size: 25,weight: .medium))
-                                            .foregroundColor(Color("Color_font"))
-                                            .multilineTextAlignment(.leading)
+                                        switch finishForFirst{
+                                        case 9...12:
+                                            Text("Инвестиционные фонды: Приблизительно n% (5-10%) портфеля можно инвестировать в активно управляемые инвестиционные фонды, которые ориентированы на высокую доходность или специализируются на определенных секторах или регионах.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 5...9:
+                                            Text("Инвестиционные фонды: Приблизительно n% (10-15%) портфеля можно инвестировать в различные инвестиционные фонды, такие как фонды смешанного типа или фонды индексного инвестирования, чтобы получить дополнительную диверсификацию и доступ к различным рынкам.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 0...5:
+                                            Text(" Инвестиционные фонды: Приблизительно n% (5-10%) портфеля можно инвестировать в консервативные инвестиционные фонды, такие как фонды с фокусом на сохранение капитала или фонды смешанного типа с умеренным риском.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        default:
+                                            Text("Erore")
+                                        }
                                         Spacer()
                                     }
                                 }
@@ -277,10 +290,25 @@ let textTest = [
                                         Spacer()
                                     }
                                     HStack {
-                                        Text("50%")
-                                            .font(.system(size: 25,weight: .medium))
-                                            .foregroundColor(Color("Color_font"))
-                                            .multilineTextAlignment(.leading)
+                                        switch finishForFirst{
+                                        case 9...12:
+                                            Text("Альтернативные инвестиции: Приблизительно n% (5-10%) портфеля можно выделить на альтернативные инвестиции, такие как недвижимость, венчурный капитал или фонды сырьевых товаров, чтобы получить дополнительную диверсификацию и потенциально высокую доходность.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 5...9:
+                                            Text("Альтернативные инвестиции: Приблизительно n% (5-10%) портфеля можно выделить на альтернативные инвестиции, такие как недвижимость, сырьевые товары или частные инвестиции, чтобы добавить дополнительную стабильность и защиту от рыночной волатильности.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        case 0...5:
+                                            Text("Денежные средства и сбережения: Приблизительно n% (20-30%) портфеля можно удерживать в безрисковых активах, таких как депозиты, сберегательные счета или денежные фонды, чтобы обеспечить ликвидность и сохранение капитала.")
+                                                .font(.system(size: 20,weight: .medium))
+                                                .foregroundColor(Color("Color_font"))
+                                                .multilineTextAlignment(.leading)
+                                        default:
+                                            Text("Erore")
+                                        }
                                         Spacer()
                                     }
                                 }
