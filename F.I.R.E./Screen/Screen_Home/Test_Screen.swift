84e0f40e44822a557368b33c29b35eca106a7094
Daniil Kornilov <peasee163@gmail.com>
2023-06-26 02:34:20 +0200

    Локализация на экране тестов где типо умеренный инвестор и тд

--- F.I.R.E./Screen/Screen_Home/Test_Screen.swift
+++ F.I.R.E./Screen/Screen_Home/Test_Screen.swift
@@ -118,7 +118,7 @@ let textTest = [
                     VStack{
                         VStack(spacing:10){
                             HStack {
-                                Text("Вы")
+                                Text("qwe0tybginm")
                                     .font(.system(size: 30,weight: .medium))
                                     .foregroundColor(Color("Color_font"))
                                     .multilineTextAlignment(.leading)
@@ -127,17 +127,17 @@ let textTest = [
                             HStack { // 1 2 5 6 7 8
                                 switch finishForFirst{
                                 case 9...12:
-                                    Text("Агрессивный\nинвестор")
+                                    Text("reqhqerh")
                                         .font(.system(size: 40,weight: .semibold))
                                         .foregroundColor(Color("Color_font_1"))
                                         .multilineTextAlignment(.leading)
                                 case 5...9:
-                                    Text("Умеренный\nинвестор")
+                                    Text("qoierhp")
                                         .font(.system(size: 40,weight: .semibold))
                                         .foregroundColor(Color("Color_font_1"))
                                         .multilineTextAlignment(.leading)
                                 case 0...5:
-                                    Text("Консервативный\nинвестор")
+                                    Text("oiwhngewn")
                                         .font(.system(size: 40,weight: .semibold))
                                         .foregroundColor(Color("Color_font_1"))
                                         .multilineTextAlignment(.leading)
@@ -153,17 +153,17 @@ let textTest = [
                             HStack {
                                 switch finishForFirst{
                                 case 9...12:
-                                    Text("Это инвестор, который предпочитает стратегию инвестирования, характеризующуюся высоким уровнем риска и ориентацией на получение максимальной возможной доходности. Они готовы вкладывать свои средства во волатильные рынки и активы с высоким потенциалом роста, даже если это связано с возможностью больших потерь.")
+                                    Text("wpieghbwne")
                                         .font(.system(size: 25,weight: .medium))
                                         .foregroundColor(Color("Color_font"))
                                         .multilineTextAlignment(.leading)
                                 case 5...9:
-                                    Text("это инвестор, который преследует баланс между риском и доходностью, не стремится к чрезмерно высоким доходам, но готов принять некоторую степень риска в своих инвестициях для достижения умеренного уровня доходности.")
+                                    Text("erhjbto")
                                         .font(.system(size: 25,weight: .medium))
                                         .foregroundColor(Color("Color_font"))
                                         .multilineTextAlignment(.leading)
                                 case 0...5:
-                                    Text("это инвестор, который предпочитает низкий уровень риска и стабильность в своих инвестициях, с акцентом на сохранение капитала.")
+                                    Text("we09ghnm")
                                         .font(.system(size: 25,weight: .medium))
                                         .foregroundColor(Color("Color_font"))
                                         .multilineTextAlignment(.leading)
@@ -177,7 +177,7 @@ let textTest = [
                             
                         
                             HStack {
-                                Text("Вам подойдут")
+                                Text("wephgweng")
                                     .font(.system(size: 35,weight: .medium))
                                     .foregroundColor(Color("Color_font_1"))
                                     .multilineTextAlignment(.leading)
@@ -190,7 +190,7 @@ let textTest = [
                                         
                                     
                                     HStack {
-                                        Text("Акций")
+                                        Text("qer0bihnm")
                                             .font(.system(size: 30,weight: .medium))
                                             .foregroundColor(Color("Color_font"))
                                             .multilineTextAlignment(.leading)
@@ -199,17 +199,17 @@ let textTest = [
                                     HStack {
                                         switch finishForFirst{
                                         case 9...12:
-                                            Text("Акции: Приблизительно n% (70-80%) портфеля можно выделить на инвестиции в акции компаний с высоким потенциалом роста, новаторские компании или активы на развивающихся рынках.")
+                                            Text("wegweg")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 5...9:
-                                            Text("Акции: Приблизительно n% (50%) портфеля может быть выделено на инвестиции в акции стабильных компаний с хорошими фундаментальными показателями и умеренным ростом.")
+                                            Text("weohnwegh")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 0...5:
-                                            Text("Дивидендные акции: Приблизительно n% (5-10%) портфеля можно выделить на инвестиции в акции компаний с устойчивыми дивидендными выплатами и долгой историей финансовой стабильности.р")
+                                            Text("weoyjmg")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
@@ -221,7 +221,7 @@ let textTest = [
                                 }
                                 VStack(spacing:10){
                                     HStack {
-                                        Text("Облигации")
+                                        Text("weignbr")
                                             .font(.system(size: 30,weight: .medium))
                                             .foregroundColor(Color("Color_font"))
                                             .multilineTextAlignment(.leading)
@@ -230,17 +230,17 @@ let textTest = [
                                     HStack {
                                         switch finishForFirst{
                                         case 9...12:
-                                            Text("Облигации: Приблизительно n% (10-20%) портфеля можно выделить на более высокодоходные, но рискованные облигации, такие как корпоративные облигации с низким рейтингом или облигации развивающихся стран.")
+                                            Text("ewrpohbynb")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 5...9:
-                                            Text("Облигации: Приблизительно n% (30%) портфеля можно выделить на облигации среднего и высокого качества, которые обеспечивают стабильный доход и уменьшают риск.")
+                                            Text("wei9ybvn")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 0...5:
-                                            Text("Облигации: Приблизительно n% (60-70%) портфеля можно выделить на инвестиции в надежные и стабильные облигации с высоким кредитным рейтингом и низким уровнем риска.")
+                                            Text("weryb")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
@@ -252,7 +252,7 @@ let textTest = [
                                 }
                                 VStack(spacing:10){
                                     HStack {
-                                        Text("Инвестиционные фонды")
+                                        Text("vr09ebhn")
                                             .font(.system(size: 30,weight: .medium))
                                             .foregroundColor(Color("Color_font"))
                                             .multilineTextAlignment(.leading)
@@ -261,17 +261,17 @@ let textTest = [
                                     HStack {
                                         switch finishForFirst{
                                         case 9...12:
-                                            Text("Инвестиционные фонды: Приблизительно n% (5-10%) портфеля можно инвестировать в активно управляемые инвестиционные фонды, которые ориентированы на высокую доходность или специализируются на определенных секторах или регионах.")
+                                            Text("wer90yihibnm")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 5...9:
-                                            Text("Инвестиционные фонды: Приблизительно n% (10-15%) портфеля можно инвестировать в различные инвестиционные фонды, такие как фонды смешанного типа или фонды индексного инвестирования, чтобы получить дополнительную диверсификацию и доступ к различным рынкам.")
+                                            Text("erwpoyghnbvm")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 0...5:
-                                            Text(" Инвестиционные фонды: Приблизительно n% (5-10%) портфеля можно инвестировать в консервативные инвестиционные фонды, такие как фонды с фокусом на сохранение капитала или фонды смешанного типа с умеренным риском.")
+                                            Text("erighbnm")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
@@ -283,7 +283,7 @@ let textTest = [
                                 }
                                 VStack(spacing:10){
                                     HStack {
-                                        Text("Альтернативные\nактивы")
+                                        Text("wthnmbewerw")
                                             .font(.system(size: 30,weight: .medium))
                                             .foregroundColor(Color("Color_font"))
                                             .multilineTextAlignment(.leading)
@@ -292,17 +292,17 @@ let textTest = [
                                     HStack {
                                         switch finishForFirst{
                                         case 9...12:
-                                            Text("Альтернативные инвестиции: Приблизительно n% (5-10%) портфеля можно выделить на альтернативные инвестиции, такие как недвижимость, венчурный капитал или фонды сырьевых товаров, чтобы получить дополнительную диверсификацию и потенциально высокую доходность.")
+                                            Text("weoygbnm43hg")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 5...9:
-                                            Text("Альтернативные инвестиции: Приблизительно n% (5-10%) портфеля можно выделить на альтернативные инвестиции, такие как недвижимость, сырьевые товары или частные инвестиции, чтобы добавить дополнительную стабильность и защиту от рыночной волатильности.")
+                                            Text("weghbnrmeet")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
                                         case 0...5:
-                                            Text("Денежные средства и сбережения: Приблизительно n% (20-30%) портфеля можно удерживать в безрисковых активах, таких как депозиты, сберегательные счета или денежные фонды, чтобы обеспечить ликвидность и сохранение капитала.")
+                                            Text("weoytgbgnvm")
                                                 .font(.system(size: 20,weight: .medium))
                                                 .foregroundColor(Color("Color_font"))
                                                 .multilineTextAlignment(.leading)
@@ -431,12 +431,12 @@ let textTest = [
                             RoundedRectangle(cornerRadius: 30)
                                 .foregroundColor(Color("Color_font_1"))
                             if textTest.count - 1 == index  {
-                                Text("Закончить")
+                                Text("we0iyhgbnvew")
                                     .font(.system(size: 25,weight: .medium))
                                     .foregroundColor(Color("Color_font_2"))
                                     .multilineTextAlignment(.center)
                             } else {
-                                Text("Дальше")
+                                Text("weoytgbgnvm")
                                     .font(.system(size: 25,weight: .medium))
                                     .foregroundColor(Color("Color_font_2"))
                                     .multilineTextAlignment(.center)
