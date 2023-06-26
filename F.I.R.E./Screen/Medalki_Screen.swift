4e3b2b7ebc65094eb599f95f98280be48c611b3c
Nikita Malykhin <malykhinwork@internet.ru>
2023-06-26 23:23:50 +0200

    ffsds

--- F.I.R.E./Screen/Medalki_Screen.swift
+++ F.I.R.E./Screen/Medalki_Screen.swift
@@ -15,14 +15,44 @@ struct Medalki_Screen: View {
     
     
     @State private var testtext1 : [String] = [
-        NSLocalizedString("word_0", comment: ""),
-        NSLocalizedString("word_1", comment: ""),
-        NSLocalizedString("word_2", comment: ""),
-        NSLocalizedString("word_0", comment: ""),
-        NSLocalizedString("word_1", comment: ""),
-        NSLocalizedString("word_2", comment: ""),
+        NSLocalizedString("SmartQues1.1", comment: ""),
+        NSLocalizedString("SmartQues1.2", comment: ""),
+        NSLocalizedString("SmartQues1.3", comment: ""),
+        NSLocalizedString("SmartQues1.4", comment: ""),
+        NSLocalizedString("SmartQues1.5", comment: ""),
+        NSLocalizedString("SmartQues1.6", comment: ""),
+        NSLocalizedString("SmartQues1.7", comment: ""),
+        NSLocalizedString("SmartQues1.8", comment: ""),
     ]
-    @State private var testtruee : [Bool] = [ true, true, true , false ,false , true
+    @State private var testtruee : [Bool] = [ false, false, false , true ,true , false, true, true
+    ]
+    
+    
+    @State private var testtext2 : [String] = [
+        NSLocalizedString("SpendWisely1.1", comment: ""),
+        NSLocalizedString("SpendWisely1.2", comment: ""),
+        NSLocalizedString("SpendWisely1.3", comment: ""),
+        NSLocalizedString("SpendWisely1.4", comment: ""),
+        NSLocalizedString("SpendWisely1.5", comment: ""),
+        NSLocalizedString("SpendWisely1.6", comment: ""),
+        NSLocalizedString("SpendWisely1.7", comment: ""),
+        NSLocalizedString("SpendWisely1.8", comment: ""),
+    ]
+    @State private var testtrue2 : [Bool] = [ false, false, true , true ,true , true, true, true
+    ]
+    
+    
+    @State private var testtext3 : [String] = [
+        NSLocalizedString("AbilityMoney1.1", comment: ""),
+        NSLocalizedString("AbilityMoney1.2", comment: ""),
+        NSLocalizedString("AbilityMoney1.3", comment: ""),
+        NSLocalizedString("AbilityMoney1.4", comment: ""),
+        NSLocalizedString("AbilityMoney1.5", comment: ""),
+        NSLocalizedString("AbilityMoney1.6", comment: ""),
+        NSLocalizedString("AbilityMoney1.7", comment: ""),
+        NSLocalizedString("AbilityMoney1.8", comment: ""),
+    ]
+    @State private var testtrue3 : [Bool] = [ true, true, true , false ,true , true, true, true
     ]
     
     
@@ -51,12 +81,12 @@ struct Medalki_Screen: View {
                                 .multilineTextAlignment(.center)
                                 .font(.system(size: 20,weight: .medium))
                                 .foregroundColor(Color("Color_font_3"))
-                            Text("eriyohnbm")
+                            Text("SmartQues1")
                                 .multilineTextAlignment(.center)
                                 .font(.system(size: 25,weight: .medium))
                                 .foregroundColor(Color("Color_font"))
                             NavigationLink {
-                                Test_View(totalQwestion: 6, Text_info: testtext1, TrueQwestion: testtruee, isMedal: true, titleTest: "Тест1", testIsFinish: $test1, testFinishHow: $test_1)
+                                Test_View(totalQwestion: 8, Text_info: testtext1, TrueQwestion: testtruee, isMedal: true, titleTest: "SmartQues1", testIsFinish: $test1, testFinishHow: $test_1)
                                 
                             } label: {
                                 
@@ -82,8 +112,102 @@ struct Medalki_Screen: View {
                             Divider()
                                 
                         } //test nomer z1
-                       
-                        
+                        VStack(spacing:25){
+                            switch test_1 {
+                            case 0:
+                                Image("medalki_nil")
+                            case 1:
+                                Image("medalki_nil")
+                            case 2:
+                                Image("medalki")
+                            case 3:
+                                Image("medalki_2")
+                            default:
+                                Text("Erorre")
+                            }
+                            Text("asdqq")
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 20,weight: .medium))
+                                .foregroundColor(Color("Color_font_3"))
+                            Text("SpendWisely1")
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 25,weight: .medium))
+                                .foregroundColor(Color("Color_font"))
+                            NavigationLink {
+                                Test_View(totalQwestion: 8, Text_info: testtext2, TrueQwestion: testtrue2, isMedal: true, titleTest: "SpendWisely1", testIsFinish: $test1, testFinishHow: $test_1)
+                                
+                            } label: {
+                                
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .padding(.vertical,-15)
+                                        .foregroundColor(Color("Color_font_1"))
+                                        .padding(.horizontal,30)
+                                    if test_1 >= 1 {
+                                        Text("qwdqwgtty")
+                                            .multilineTextAlignment(.center)
+                                            .font(.system(size: 20,weight: .medium))
+                                            .foregroundColor(Color("Color_font_2"))
+                                    } else {
+                                        Text("Jowvv")
+                                            .multilineTextAlignment(.center)
+                                            .font(.system(size: 20,weight: .medium))
+                                            .foregroundColor(Color("Color_font_2"))
+                                    }
+                                    
+                                }
+                            }.padding(.bottom,15)
+                            Divider()
+                                
+                        } //test nomer z2
+                        VStack(spacing:25){
+                            switch test_1 {
+                            case 0:
+                                Image("medalki_nil")
+                            case 1:
+                                Image("medalki_nil")
+                            case 2:
+                                Image("medalki")
+                            case 3:
+                                Image("medalki_2")
+                            default:
+                                Text("Erorre")
+                            }
+                            Text("asdqq")
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 20,weight: .medium))
+                                .foregroundColor(Color("Color_font_3"))
+                            Text("AbilityMoney1")
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 25,weight: .medium))
+                                .foregroundColor(Color("Color_font"))
+                            NavigationLink {
+                                Test_View(totalQwestion: 8, Text_info: testtext3, TrueQwestion: testtrue3, isMedal: true, titleTest: "AbilityMoney1", testIsFinish: $test1, testFinishHow: $test_1)
+                                
+                            } label: {
+                                
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .padding(.vertical,-15)
+                                        .foregroundColor(Color("Color_font_1"))
+                                        .padding(.horizontal,30)
+                                    if test_1 >= 1 {
+                                        Text("qwdqwgtty")
+                                            .multilineTextAlignment(.center)
+                                            .font(.system(size: 20,weight: .medium))
+                                            .foregroundColor(Color("Color_font_2"))
+                                    } else {
+                                        Text("Jowvv")
+                                            .multilineTextAlignment(.center)
+                                            .font(.system(size: 20,weight: .medium))
+                                            .foregroundColor(Color("Color_font_2"))
+                                    }
+                                    
+                                }
+                            }.padding(.bottom,15)
+                            Divider()
+                                
+                        } //test nomer z2
                         
                     }.padding(.horizontal,15)
                         .padding(.top,15)
