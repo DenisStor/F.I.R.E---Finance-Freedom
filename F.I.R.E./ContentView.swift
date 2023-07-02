b6247e89f116f49414cd4713fe3eecd47094ddb7
DenShizik <denisdikarvit@gmail.com>
2023-07-02 15:12:56 +0200

    fix

--- F.I.R.E./ContentView.swift
+++ F.I.R.E./ContentView.swift
@@ -3,17 +3,35 @@ import SwiftUI
 struct ContentView: View {
     @AppStorage ("currentPage") var currentPage : Int = 1
     
-   
-
+    @State private var IsLoading = false
+    @State private var index = 0
+    @State private var timer = Timer.TimerPublisher(interval: 0.5, runLoop: .current, mode: .common).autoconnect()
     var body: some View {
         ZStack{
-        
-            Start_Screen()
-                .offset(x: currentPage == 1 ? 0 :  -700)
-        
-            Bar_Screen()
-                .offset(x: currentPage == 2 ? 0 :  700)
-        
+           
+                Start_Screen()
+                    .offset(x: currentPage == 1 ? 0 :  -700)
+                
+                Bar_Screen()
+                    .offset(x: currentPage == 2 ? 0 :  700)
+           
+               
+            ZStack {
+                Color("Color_font_1")
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    Image("name_name_name")
+                        .onReceive(timer) { i in
+                            index += 1
+                            if index == 2 {
+                                IsLoading = true
+                            }
+                        }
+                }
+            }.offset(y: IsLoading ? 1000 : 0)
+            .animation(.spring(), value: IsLoading)
+            
         
         }.animation(.easeInOut, value: currentPage )
             
